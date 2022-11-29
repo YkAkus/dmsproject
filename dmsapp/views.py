eb02976559a1a921a65667c978395bce2c2ba681
Badal Kumar <thebadalkumar@zohomail.in>
2022-11-29 21:52:22 +0530

    Sharable Link

--- dmsapp/views.py
+++ dmsapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import FileForm
 from django.views import View
 from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from .models import File
 class FileUploader(View):
     def get(self, request, *args, **kwargs):
