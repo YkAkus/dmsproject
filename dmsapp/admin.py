82b4510eb2b0226b335140ed9a556e81ef2d1ff3
yk9335 <yadvendra.yadav@akus.co.in>
2022-11-29 17:01:02 +0530

    DMSNew

---
+++ dmsapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib.auth import models
+from dmsapp.models import registers
+
+
+# Register your models here.
+admin.site.register(registers)
---
+++ dmsapp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class registers(models.Model):
+    fname=models.CharField(max_length=50)
+    lname=models.CharField(max_length=50)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
+    class Mata:
+        managed = True
