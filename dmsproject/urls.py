5afc1f00a4b42913f8c2a077d42026a988e93d83
Badal Kumar <thebadalkumar@zohomail.in>
2022-11-29 21:51:15 +0530

    Sharable Link

--- dmsproject/urls.py
+++ dmsproject/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from dmsapp.views import viewFile
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('',include('dmsapp.urls'))
+    path('',include('dmsapp.urls')),
+    path('<str:url>', viewFile, name="view-file"),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
