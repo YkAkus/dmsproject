2ce1fd017094b19d2a2c9bd1b844668cc4f5dab1
Badal Kumar <thebadalkumar@zohomail.in>
2022-11-29 21:45:48 +0530

    Few More Functions

--- dmsapp/urls.py
+++ dmsapp/urls.py
@@ -3,4 +3,11 @@ from dmsapp import views
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('', login_required(views.FileUploader.as_view()),name='index'),
+    path('files/', views.FileView, name="file-view"),
+    path('favourite/', views.favFileView, name="fav-file-view"),
+    path('del-files/', views.deleteFile, name="del-file"),
+    path('remove-files/', views.removeFile, name="remove-file"),
+    path('make-fav/', views.makeFav, name="fav-file"),
+    path('restore-file/', views.restoreFile, name="restore-file"),
+    path('trash/', views.remFileView, name="trash"),
 ]
--- dmsapp/views.py
+++ dmsapp/views.py
@@ -8,7 +8,7 @@ class FileUploader(View):
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect("login")
-        obj = File.objects.filter(user = request.user)
+        obj = File.objects.filter(user = request.user, is_delete=False)
         form = FileForm()
         return render(request, 'index.html', {'form': form, 'files':obj})
 
@@ -29,3 +29,66 @@ class FileUploader(View):
             return JsonResponse({'data':'Data uploaded'})
         else:
             return JsonResponse({'data':'Something went wrong!!'})
+
+def viewFile(request, url):
+    file = get_object_or_404(File,url = url, is_delete=False)
+    return render(request, "fileUploader/file.html", {"file":file})
+
+@login_required
+def FileView(request):
+    files = File.objects.filter(user = request.user, is_delete=False)
+    context = {
+        "files" : files
+    }
+    return render(request, "fileUploader/myfiles.html", context)
+
+@login_required
+def favFileView(request):
+    files = File.objects.filter(user = request.user, is_delete=False, is_fav=True)
+    context = {
+        "files" : files
+    }
+    return render(request, "fileUploader/myfavfiles.html", context)
+
+@login_required
+def remFileView(request):
+    files = File.objects.filter(user = request.user, is_delete=True)
+    context = {
+        "files" : files
+    }
+    return render(request, "fileUploader/myremovedfiles.html", context)
+
+@login_required
+def removeFile(request):
+    id = request.POST["id"]
+    file = File.objects.get(id = id)
+    file.is_delete = True
+    file.save()
+    return JsonResponse({"data":True})
+
+@login_required
+def deleteFile(request):
+    id = request.POST["id"]
+    file = File.objects.get(id = id)
+    file.delete()
+    return JsonResponse({"data":True})
+
+@login_required
+def restoreFile(request):
+    id = request.POST["id"]
+    file = File.objects.get(id = id)
+    file.is_delete = False
+    file.is_fav = False
+    file.save()
+    return JsonResponse({"data":True})
+
+@login_required
+def makeFav(request):
+    id = request.POST["id"]
+    file = File.objects.get(id = id)
+    if file.is_fav:
+        file.is_fav = False
+    else:
+        file.is_fav = True
+    file.save()
+    return JsonResponse({"data":file.is_fav})
