526549e3487068c90e26305e06301d7603188e8b
yk9335 <yadvendra.yadav@akus.co.in>
2022-12-27 15:57:31 +0530

    password reset

--- dmsproject/settings.py
+++ dmsproject/settings.py
@@ -130,8 +130,16 @@ USE_I18N = True
 
 USE_TZ = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-ACCOUNT_EMAIL_VERIFICATION = "none"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# ACCOUNT_EMAIL_VERIFICATION = "none"
+# ....
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'akusdms2022@gmail.com'
+EMAIL_HOST_PASSWORD = 'whcorhaiobmcebrv'
+EMAIL_USE_TLS = True
+# ....
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
